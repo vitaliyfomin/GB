22ff16e4ea5f39316726ab969871f762eac5dfd6
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-05 23:51:38 +0500

    task02 fine

--- lesson02/task02/Program.cs
+++ lesson02/task02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.Clear();
+Console.Write("Введите первое число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число B: ");
+int b = int.Parse(Console.ReadLine());
+
+int max, min;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine($"Максимальное число: {max}");
+Console.WriteLine($"Минимальное число: {min}");
