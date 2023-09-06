d65ebd3d082009e096cccf8d518a99c385e52545
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-06 20:36:30 +0500

    task08 fine

---
+++ lesson02/task08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.Clear();
+
+Console.Write($"Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Чeтные числа от 1 до " + N + ":");
+
+for (int i = 2; i <= N; i += 2)
+{
+Console.Write(i + $" ");
+}
