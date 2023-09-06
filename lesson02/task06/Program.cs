d2dd6607c2d34b14916b7ecf5f8b3c56673fd58c
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-06 20:18:56 +0500

    task06 fine

---
+++ lesson02/task06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+Console.Clear();
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    
+    Console.WriteLine($"Число {number} является четным.");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является четным.");
+}
+
+
