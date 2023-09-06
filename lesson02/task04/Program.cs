6c481c347b40f149d785ed4d14b045e88d511e74
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-06 11:18:36 +0500

    Черновой вариант 4 задачи. Может и правельный? Нужно проверить!

---
+++ lesson02/task04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max;
+if (a > b)
+{
+    max = a;
+}
+if (b > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+
+Comsole.WriteLine($"Максимальное число: {max}");
