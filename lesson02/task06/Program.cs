fecfab2d9a72148b10b93b4160deeb89556d8635
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-06 13:31:02 +0500

    task06 beta

---
+++ lesson02/task06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+Console.Clear();
+
+Console.Write($"Введите число A: ");
+double a = int.Parse(Console.ReadLine());
+Console.Write($"Введите число B: ");
+double b = int.Parse(Console.ReadLine());
+
+int result = (a/b==a);
+
+if result;
+{
+    
+    Console.WriteLine($"Число {a} является четным.");
+}
+else
+{
+    Console.WriteLine($"Число {a} не является четным.");
+}
+
+
