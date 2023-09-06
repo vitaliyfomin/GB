81bd890215a8947fda336ab02a51026ee79231f4
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-06 20:02:51 +0500

    del_mod task04

---
+++ lesson02/task04/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите первое число: ");
+double numb1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numb2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double numb3 = Convert.ToDouble(Console.ReadLine());
+
+double max = Math.Max(numb1, Math.Max(numb2, numb3));
