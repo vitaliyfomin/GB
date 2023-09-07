1ecf72e8961e07631a544848d66dd609071a3f72
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-07 22:07:31 +0500

    task 15 fine

--- LESSON04/task15/Program.cs
+++ LESSON04/task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.Clear();
+
+Console.WriteLine($"Введите день недели (1 - понедельник, 2 - вторник,.... 7 - воскресенье)");
+
+int dayweek = Convert.ToInt32(Console.ReadLine());
+
+if(dayweek == 6 || dayweek == 7)
+
+{
+    Console.WriteLine($"Да, это выходной день.");
+} 
+
+else
+
+{
+    Console.WriteLine($"Нет, это не выходной день");
+}
