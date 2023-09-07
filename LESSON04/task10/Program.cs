7be23f52f1ef2d091b03fc8ebad4fc14554c62a7
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-07 21:30:11 +0500

    task10 fine

--- LESSON04/task10/Program.cs
+++ LESSON04/task10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.Clear();
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 100 && number < 999)
+
+{
+    int secondDigit = (number  / 10 % 10);
+    Console.WriteLine($"Вторая цифра числа: " + secondDigit);
+}
+
+else
+
+{
+    Console.WriteLine($"Вы ввели не верные цифры");
+}
+
