9ca46f6dc06217eaa743141512d9c41d4b0514a0
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-08 20:27:30 +0500

    task27

--- LESSON07/task27/Program.cs
+++ LESSON07/task27/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+
+        int sum = CalculateDigitSum(input);
+
+        Console.WriteLine($"Сумма цифр в числе {input} равна {sum}");
+    }
+
+    static int CalculateDigitSum(string number)
+    {
+        int sum = 0;
+
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                sum += int.Parse(digit.ToString());
+            }
+        }
+
+        return sum;
+    }
+}
