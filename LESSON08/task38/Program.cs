0c49a1e9104bf6406b68562d4c73576a1d2f3718
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-08 21:45:15 +0500

    task38 fin

--- LESSON08/task38/Program.cs
+++ LESSON08/task38/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+// Очистка терминала
+        Console.Clear();
+// Создаем массив вещественных чисел
+        double[] array = { 3, 7, 22, 2, 78 };
+
+// Находим минимальный и максимальный элементы в массиве
+        double min = array[0];
+        double max = array[0];
+
+        foreach (double number in array)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+// Находим разницу между максимальным и минимальным элементами
+        double difference = max - min;
+
+        Console.WriteLine($"Минимальный элемент: {min}");
+        Console.WriteLine($"Максимальный элемент: {max}");
+        Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
+    }
+}
