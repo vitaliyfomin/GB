f51a92d7f4ea69c2067dfc89ec6a1f53db3d0814
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-08 20:33:09 +0500

    task25 fin

--- LESSON07/task25/Program.cs
+++ LESSON07/task25/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear(); // Очистка терминала
+
+        Console.Write("Введите число A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите степень B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        int result = CalculatePower(A, B);
+        Console.WriteLine($"{A}^{B} = {result}");
+    }
+
+    static int CalculatePower(int A, int B)
+    {
+        int result = 1;
+
+        for (int i = 1; i <= B; i++)
+        {
+            result *= A;
+        }
+
+        return result;
+    }
+}
