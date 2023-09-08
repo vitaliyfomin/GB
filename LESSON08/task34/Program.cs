95f6bada8608c577467e300014f61462658373f0
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-08 21:35:18 +0500

    task34

--- LESSON08/task34/Program.cs
+++ LESSON08/task34/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+// Очистка терминала
+        Console.Clear();
+
+        Random random = new Random();
+// Создаем массив из 4 элементов
+        int[] array = new int[4];
+
+// Заполняем массив случайными трехзначными числами
+        for (int i = 0; i < 4; i++)
+        {
+// Генерируем случайное трехзначное число
+            array[i] = random.Next(100, 1000);
+        }
+
+// Выводим массив на экран
+        Console.WriteLine("Массив:");
+        Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+// Считаем количество четных чисел в массиве
+        int countEven = 0;
+        foreach (int number in array)
+        {
+            if (number % 2 == 0)
+            {
+                countEven++;
+            }
+        }
+
+        Console.WriteLine($"Количество четных чисел в массиве: {countEven}");
+    }
+}
