ec2e97a595d9340d18c722e9d54487bce74367e0
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-10 15:14:28 +0500

    task66 fin

--- LESSON15/task66/Program.cs
+++ LESSON15/task66/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int sum = GetSumOfNaturalNumbersInRange(M, N);
+        Console.WriteLine($"Сумма натуральных чисел от {M} до {N}: {sum}");
+    }
+
+    static int GetSumOfNaturalNumbersInRange(int M, int N)
+    {
+        if (M <= 0 || N <= 0)
+        {
+            // Возвращаем 0, если M или N не являются натуральными числами
+            return 0;
+        }
+
+        int sum = 0;
+        for (int i = M; i <= N; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
