14b3a1ebd123f530ee9f9359c277c9330a1330a3
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-10 15:23:27 +0500

    task68 fin

--- LESSON15/task68/Program.cs
+++ LESSON15/task68/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        Console.Write("Введите число M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите число N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        AkkermanFunction(m, n);
+
+    }
+
+    // вызов функции Аккермана
+    static void AkkermanFunction(int m, int n)
+    {
+        Console.Write($"A(m,n)= {Akkerman(m, n)}");
+    }
+
+    // функция Аккермана
+    static int Akkerman(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0 && m > 0)
+        {
+            return Akkerman(m - 1, 1);
+        }
+        else
+        {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
