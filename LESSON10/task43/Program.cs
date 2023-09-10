d2974c6d251af235b53ad0c489d0a3b104bb0aa4
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-10 11:29:20 +0500

    task43 fin

--- LESSON10/task43/Program.cs
+++ LESSON10/task43/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+// Очищаем терминал перед началом выполнения программы
+        Console.Clear();
+
+        Console.Write("Введите значение k1: ");
+        double k1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b1: ");
+        double b1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k2: ");
+        double k2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b2: ");
+        double b2 = double.Parse(Console.ReadLine());
+
+// Решаем систему уравнений
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+
+        Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
+    }
+}
