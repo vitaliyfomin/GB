f9e52fe5d1f412b1d94cc179321fb291326718c4
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-10 15:11:17 +0500

    task64 fin

--- LESSON15/task64/Program.cs
+++ LESSON15/task64/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(GetNaturalNumbersInRange(M, N));
+    }
+
+    static string GetNaturalNumbersInRange(int M, int N)
+    {
+        if (M <= 0 || N <= 0)
+        {
+            return "Введены некорректные значения. M и N должны быть натуральными числами.";
+        }
+
+        string result = "";
+        for (int i = M; i <= N; i++)
+        {
+            result += i.ToString();
+            if (i < N)
+            {
+                result += ", ";
+            }
+        }
+        return $"\"{result}\"";
+    }
+}
