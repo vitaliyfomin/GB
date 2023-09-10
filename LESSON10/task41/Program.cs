92d15c1a0a3e567368b450a8dcee0b50b3ad1ce9
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-09-10 11:23:28 +0500

    task41 fin

--- LESSON10/task41/Program.cs
+++ LESSON10/task41/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+// Очищаем терминал перед началом выполнения программы
+        Console.Clear();
+
+        Console.Write("Введите числа, разделенные запятыми: ");
+// Считываем строку, содержащую числа
+        string input = Console.ReadLine();
+
+// Разделяем строку на массив чисел, используя запятые как разделители
+        string[] numbers = input.Split(',');
+
+// Инициализируем счетчик чисел больше 0
+        int count = 0;
+
+// Перебираем числа в массиве
+        foreach (string numStr in numbers)
+        {
+// Пробуем преобразовать строку в число
+            if (double.TryParse(numStr.Trim(), out double num))
+            {
+                if (num > 0)
+                {
+// Увеличиваем счетчик, если число больше 0
+                    count++;
+                }
+            }
+        }
+// Выводим результат
+        Console.WriteLine($"Количество чисел больше 0: {count}");
+    }
+}
